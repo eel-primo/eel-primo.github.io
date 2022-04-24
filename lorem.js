09995d0cc9a7588b30ecf250248d4fd593b28be7
eel-primo <47497416+eel-primo@users.noreply.github.com>
2022-04-24 17:51:14 +0300

    Update lorem.js

--- lorem.js
+++ lorem.js
@@ -1,5 +1,10 @@
-var xmlHttp = new XMLHttpRequest();
+// Get text filler from API
+let xmlHttp = new XMLHttpRequest();
 xmlHttp.open( "GET", "https://baconipsum.com/api/?type=all-meat&sentences=1&format=text", false );
 xmlHttp.send( null );
-var msg = document.getElementById("lorem");
-msg.innerHTML = xmlHttp.responseText;
+
+// Get the element and change the text to the query result
+let lorem = document.getElementById("lorem");
+lorem.innerHTML = xmlHttp.responseText;
+
+// That's all code!
